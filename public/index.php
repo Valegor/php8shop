be949bcadaf9529bfce520517a75d8dfad3a8632
Valegor <medifit2000@yandex.ru>
2022-03-16 18:34:26 +0300

    add container

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+
+if (PHP_MAJOR_VERSION < 8) {
+    die('Необходима версия PHP >= 8');
+}
+
+require_once dirname(__DIR__) . '/config/init.php';
+
+new \wfm\App();
+
+// echo \wfm\App::$app->getProperty('pagination');
+//\wfm\App::$app->setProperty('test', 'TEST');
+var_dump(\wfm\App::$app->getProperties());
